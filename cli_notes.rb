96e5cfb67ce2ef436188b3c034192cb70b74a9f0
alexencinas <shoogiee123@gmail.com>
2018-06-06 22:28:19 +0000

    automatically backed up by learn

--- cli_notes.rb
+++ cli_notes.rb
@@ -7,13 +7,25 @@
 # puts "I love #{answer}!"
 
 
-game_number = "75"
+# game_number = "75"
 
-puts "Please input your first name, Tribute:"
-first_name = gets.chomp
-puts "Please input your last name:"
-last_name = gets.chomp
+# puts "Please input your first name, Tribute:"
+# first_name = gets.chomp
+# puts "Please input your last name:"
+# last_name = gets.chomp
 
-puts "Welcome to the #{game_number}th Hunger Games, #{first_name} #{last_name}."
+# puts "Welcome to the #{game_number}th Hunger Games, #{first_name} #{last_name}."
 
-#make a program that taks in a single integer and prints 'odd' or 'even'
+#make a program that taks in a single integer and prints 'odd' or 'even' The program should run forever, always waiting for input after it processes the previous".
+
+
+puts "Enter a number"
+choice_number = gets.chomp
+
+if (choice_number)%2==0
+  puts "Even"
+end
+
+if (choice_number)%2!=0
+  puts "Odd"
+end 
