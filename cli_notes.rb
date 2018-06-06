2fb54064fdde1c02a9901a7eb3b3ea49a902ba73
alexencinas <shoogiee123@gmail.com>
2018-06-06 22:22:50 +0000

    automatically backed up by learn

--- cli_notes.rb
+++ cli_notes.rb
@@ -1,7 +1,17 @@
-puts "What's your favorite animal?"
-# answer = gets.chomp
-# puts "I love #{answer}!"
-#gets takes in the user input and then starts a new line 
+# puts "What's your favorite animal?"
+# # answer = gets.chomp
+# # puts "I love #{answer}!"
+# #gets takes in the user input and then starts a new line 
 
-answer = gets.chomp
-puts "I love #{answer}!"
+# answer = gets.chomp : makes the sentence on one line 
+# puts "I love #{answer}!"
+
+
+game_number = "75"
+
+puts "Please input your first name, Tribute:"
+first_name = gets.chomp
+puts "Please input your last name:"
+last_name = gets.chomp
+
+puts "Welcome to the #{game_number}th Hunger Games, #{first_name} #{last_name}"
